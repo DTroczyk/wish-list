6449d93915c39b169f6a8c6490646bc4d721f16d
Dominik Tracz <DTroczyk@gmail.com>
2022-05-04 14:24:40 +0200

    Remove autologin to sample account

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,7 +33,7 @@ export class AppComponent implements OnInit, OnDestroy {
         (count) => (this.unreadMessagesCount = count)
       )
     );
-    this.userService.login('dtroczyk', 'haslo');
+    // this.userService.login('janedoe', 'haslo');
   }
 
   ngOnDestroy(): void {
