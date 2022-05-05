9d8da1ab56f06035363e523fc34697e57f986e37
Dominik Tracz <DTroczyk@gmail.com>
2022-05-05 13:27:22 +0200

    Add wish service tests

--- src/app/services/wish/wish.service.spec.ts
+++ src/app/services/wish/wish.service.spec.ts
@@ -1,4 +1,6 @@
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import Wish from 'src/app/models/wish';
+import { SampleWishes } from 'src/app/pages/main/sample-wishes';
 
 import { WishService } from './wish.service';
 
@@ -13,4 +15,79 @@ describe('WishService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should be set wishes', fakeAsync(() => {
+    let wishes: Wish[] = [];
+    let subs = service.wishesSubject.subscribe((res) => (wishes = res));
+    tick(10000);
+    expect(wishes.length).toEqual(0);
+
+    service.setWishes([...SampleWishes]);
+    tick(10000);
+    expect(wishes.length).toEqual(7);
+
+    subs.unsubscribe();
+  }));
+
+  it('should add the wish', fakeAsync(() => {
+    let wishes: Wish[] = [];
+    let subs = service.wishesSubject.subscribe((res) => (wishes = res));
+    service.setWishes([...SampleWishes]);
+    tick(10000);
+    expect(wishes.length).toEqual(7);
+
+    let newWish: Wish = {
+      id: 16,
+      name: 'New Wish',
+      description: 'Newest wish on the list',
+      userId: 'user',
+    };
+    service.addOrEditWish(newWish);
+    tick(10000);
+    expect(wishes.length).toEqual(8);
+    let foundWish = wishes.find((w) => w.id === 8);
+    expect(foundWish).toEqual(newWish);
+
+    subs.unsubscribe();
+  }));
+
+  it('should edit the wish', fakeAsync(() => {
+    let wishes: Wish[] = [];
+    let subs = service.wishesSubject.subscribe((res) => (wishes = res));
+    service.setWishes([...SampleWishes]);
+    tick(10000);
+    expect(wishes.length).toEqual(7);
+
+    let orgWish = wishes[Math.floor((Math.random() * 10000) % wishes.length)];
+    let editedWish = { ...orgWish };
+    editedWish.name = 'Sample wish';
+    editedWish.price = 587;
+    editedWish.userId = 'testuser95621';
+    expect(orgWish).not.toEqual(editedWish);
+
+    service.addOrEditWish(editedWish);
+    tick(10000);
+    expect(wishes.length).toEqual(7);
+
+    let foundWish = wishes.find((w) => w.id === orgWish.id);
+    expect(foundWish).not.toBeUndefined();
+    expect(foundWish).toEqual(editedWish);
+    expect(foundWish).not.toEqual(orgWish);
+  }));
+
+  it('should remove the wish', fakeAsync(() => {
+    let wishes: Wish[] = [];
+    let subs = service.wishesSubject.subscribe((res) => (wishes = res));
+    service.setWishes([...SampleWishes]);
+    tick(10000);
+    expect(wishes.length).toEqual(7);
+    let wishToRemove =
+      wishes[Math.floor((Math.random() * 10000) % wishes.length)];
+
+    service.deleteWish(wishToRemove.id);
+    tick(10000);
+    expect(wishes.length).toEqual(6);
+    let foundWish = wishes.find((w) => w.id === wishToRemove.id);
+    expect(foundWish).toBeUndefined();
+  }));
 });
--- src/app/services/wish/wish.service.ts
+++ src/app/services/wish/wish.service.ts
@@ -20,22 +20,23 @@ export class WishService {
   }
 
   addOrEditWish(wish: Wish) {
-    if (wish && this.wishes) {
-      if (wish.price) wish.price = wish.price * 100;
+    setTimeout(() => {
+      if (wish && this.wishes) {
+        if (wish.price) wish.price = wish.price * 100;
 
-      if (this.wishes.find((w) => w.id === wish.id)) {
-        this.wishes = this.wishes.map((w) =>
-          w.id === wish.id ? (w = wish) : w
-        );
-      } else {
-        // Set id
-        if (this.wishes) wish.id = this.wishes[this.wishes.length - 1].id + 1;
+        if (this.wishes.find((w) => w.id === wish.id)) {
+          this.wishes = this.wishes.map((w) =>
+            w.id === wish.id ? (w = wish) : w
+          );
+        } else {
+          // Set id
+          if (this.wishes) wish.id = this.wishes[this.wishes.length - 1].id + 1;
 
-        this?.wishes.push(wish);
+          this?.wishes.push(wish);
+        }
+        this.wishesSubject.next([...this.wishes]);
       }
-
-      this.wishesSubject.next([...this.wishes]);
-    }
+    }, 1000);
   }
 
   deleteWish(wishId: number) {
